ecc72b6535cb7b3a0f95b863cef3f55238ca408f
Aybe <aybe@users.noreply.github.com>
2018-04-07 19:38:44 +0200

    Added experimental integer types like ones in Boost.

---
+++ BinaryExtensions/Experimental/Int16BE.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using BinaryExtensions.Annotations;
+
+// ReSharper disable once CheckNamespace
+namespace System
+{
+    [PublicAPI]
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 2)]
+    public struct Int16BE
+    {
+        public readonly short Value;
+
+        public Int16BE(short value)
+        {
+            Value = value;
+        }
+
+        public Int16BE([NotNull] BinaryReader reader)
+        {
+            if (reader == null)
+                throw new ArgumentNullException(nameof(reader));
+
+            Value = reader.ReadInt16();
+        }
+
+        public static implicit operator Int16BE(short value)
+        {
+            return new Int16BE(value);
+        }
+
+        public static implicit operator short(Int16BE value)
+        {
+            return value.Value.ToEndian(Endianness.BigEndian);
+        }
+
+        public override string ToString()
+        {
+            return ((short) this).ToString();
+        }
+    }
+}
---
+++ BinaryExtensions/Experimental/Int32BE.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using BinaryExtensions.Annotations;
+
+// ReSharper disable once CheckNamespace
+namespace System
+{
+    [PublicAPI]
+    [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 4)]
+    public struct Int32BE
+    {
+        public readonly int Value;
+
+        public Int32BE(int value)
+        {
+            Value = value;
+        }
+
+        public Int32BE([NotNull] BinaryReader reader)
+        {
+            if (reader == null)
+                throw new ArgumentNullException(nameof(reader));
+
+            Value = reader.ReadInt32();
+        }
+
+        public static implicit operator Int32BE(int value)
+        {
+            return new Int32BE(value);
+        }
+
+        public static implicit operator int(Int32BE value)
+        {
+            return value.Value.ToEndian(Endianness.BigEndian);
+        }
+
+        public override string ToString()
+        {
+            return ((int) this).ToString();
+        }
+    }
+}
